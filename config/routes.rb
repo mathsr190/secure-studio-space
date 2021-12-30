a0e870c4b700b852c45694c2134919a98e82a853
mathsr190 <math.sr.190@gmail.com>
2021-12-30 16:11:46 +0900

    フロント実装の準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'studios/index'
   devise_for :managers
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "studios#index"
 end
