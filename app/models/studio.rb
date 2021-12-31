c7cb028e728285b1eb14d234329bc6afe166f4c3
mathsr190 <math.sr.190@gmail.com>
2022-01-01 06:58:24 +0900

    画像追加機能

--- app/models/studio.rb
+++ app/models/studio.rb
@@ -4,5 +4,16 @@ class Studio < ApplicationRecord
   belongs_to :prefecture
   has_many :studio_bdays, dependent: :destroy
   has_many :bdays, through: :studio_bdays
-  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+  has_one_attached :image
+  with_options presence: true do
+    validates :introduction
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :city
+    validates :address
+    validates :business_hours_start
+    validates :business_hours_end
+    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'is invalid' }
+    validates :manager_id
+  end
 end
