d78ad5a3ce95eca450d1b1f07bd24107d70afc5f
mathsr190 <math.sr.190@gmail.com>
2022-02-10 21:23:18 +0900

    rubocoop

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = current_user
-    @reservations = @user.reservations.includes(:reservation_detail, space: {studio: :manager})
+    @reservations = @user.reservations.includes(:reservation_detail, space: { studio: :manager })
   end
 end
